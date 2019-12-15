59cf70905b73325ce66a357d92643976447573b6
lei wang <wanglei1@pcuu.com>
2019-12-15 18:47:14 +0800

    兼容false情况下获取code

--- net/src/main/java/com/wang/net/model/JudgeResource.kt
+++ net/src/main/java/com/wang/net/model/JudgeResource.kt
@@ -3,11 +3,17 @@ package com.wang.net.model
 /****
  * 这里用来对接口返回结果进行判断
  * **/
-data class JudgeResource<T>(val status: Boolean, val data: T?, val message: String?) {
+data class JudgeResource<T>(
+    val status: Boolean,
+    val data: T?,
+    val message: String?,
+   val errCode: String? = null
+) {
     companion object {
 
-       open fun <T> success(data: T?) = JudgeResource(true, data, null)
+        open fun <T> success(data: T?) = JudgeResource(true, data, null)
 
-        open fun <T> error(msg: String?, data: T? = null) = JudgeResource(false, data, msg)
+        open fun <T> error(msg: String?, data: T? = null, errCode: String? = null) =
+            JudgeResource(false, data, msg,errCode)
     }
 }
